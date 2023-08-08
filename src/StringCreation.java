16f46180ed081e0f415f1887548169010d699311
Sarthak Giri <giri.sarthak1@gmail.com>
2023-08-08 19:33:30 +0530

    Initial commit

---
+++ src/StringCreation.java
@@ -0,0 +1,15 @@
+public class StringCreation {
+    public static void main(String[] args){
+        //creating a string by java string literal
+        String str = "ModernIndia ";
+        char[] arrch ={'G','r','e','a','t','L','e','a','r','n','i','n','g'};
+        //converting the char array arrch[] to string str2
+        String str2 = new String(arrch);
+        //creating another java string ‘str3’ by using new keyword
+        String str3 = new String("String Example");
+        //Displaying all the three strings
+        System.out.println(str);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
